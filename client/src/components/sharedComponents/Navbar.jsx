66bccacdc95ce957adb6695928b10ac33889fffb
gajju44 <gajjunaphade@gmail.com>
2025-09-05 12:45:05 +0530

    increase font of navbar

--- client/src/components/sharedComponents/Navbar.jsx
+++ client/src/components/sharedComponents/Navbar.jsx
@@ -8,23 +8,23 @@ function Navbar() {
        <div className="flex items-center gap-2">
          {/* Logo Placeholder (replace with SVG/logo) */}
          <div className="w-6 h-6 bg-black rounded-md"></div>
-         <span className="text-lg font-semibold text-gray-700">Giacomo</span>
+         <span className="text-xl font-semibold text-gray-700">Giacomo</span>
          {/* <span className="text-gray-400 text-lg">/</span> */}
-         <span className="text-lg font-semibold text-gray-700">Rotella</span>
+         <span className="text-xl font-semibold text-gray-700">Rotella</span>
        </div>
  
        {/* Right Section */}
        <div className="md:flex hidden items-center gap-6">
-         <a href="#" className="text-sm font-medium text-gray-700 hover:text-black">
+         <a href="#" className=" font-medium text-gray-700 hover:text-black">
            Home
          </a>
-         <a href="#" className="text-sm font-medium text-gray-500 hover:text-black">
+         <a href="#" className=" font-medium text-gray-500 hover:text-black">
            Blogs
          </a>
          {/* <a href="#" className="text-sm font-medium text-gray-700 hover:text-black">
            Italia 
          </a> */}
-         <a href="#" className="text-sm font-medium text-gray-500 hover:text-black">
+         <a href="#" className=" font-medium text-gray-500 hover:text-black">
            Services
          </a>
        </div>
