23d8c055b6cd368e4aea53efd9b727ede58c9fb9
gajju44 <gajjunaphade@gmail.com>
2025-11-29 00:37:15 +0530

    Fractional2

--- client/src/components/MobileHomePage/Fractional2.jsx
+++ client/src/components/MobileHomePage/Fractional2.jsx
@@ -15,7 +15,7 @@ const Fractional2 = React.memo(function Fractional2() {
     
                   <p className=' text-3xl font-semibold md:text-3xl   text-[#181818] slide-up stagger-2' ref={useViewportAnimation()}
                    style={{ fontSize: "clamp(18px, 4vw, 40px)" }}>
-                    My <Highlight className=' text-white leading-tight '> fractional marketing consulting package </Highlight> includes everything from strategy to execution:
+                    My <Highlight className=' text-white leading-tight '> fractional marketing consulting package </Highlight> includes everything from strategy to execution
                   </p>
                                {/* <button> <ArrowBigDown size={40} fill='#911c28' color='#911c28' /></button> */}
 
