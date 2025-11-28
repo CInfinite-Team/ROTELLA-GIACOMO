dfaed1887d110cd73b4c0b70481417f76dde19b7
gajju44 <gajjunaphade@gmail.com>
2025-11-29 00:27:46 +0530

    text changes

--- client/src/components/MobileHomePage/Fractional.jsx
+++ client/src/components/MobileHomePage/Fractional.jsx
@@ -5,13 +5,13 @@ const Fractional = React.memo(function Fractional() {
   return (
     <div className='flex flex-col bg-[#f5f5f5]  gap-5 min-h-[80vh] lg:min-h-[100vh]  items-center justify-center'>
        <p className='font-bold  slide-up stagger-1  !leading-tight  mt-10 ' ref={useViewportAnimation()}
-             style={{ fontSize: "clamp(30px, 6vw, 57px)" }}
+             style={{ fontSize: "clamp(30px, 4vw, 57px)" }}
 >
                       What is a fractional marketing consultant?
                     </p>
       
                     <p className='  font-semibold  slide-up stagger-2 text-[#981818] 2xl:mt-4 ' ref={useViewportAnimation()}
-                    style={{ fontSize: "clamp(16px, 5.5vw, 40px)" }}>
+                    style={{ fontSize: "clamp(16px, 3vw, 40px)" }}>
                       As a fractional marketing consultant, my team and I work with you on a part-time and long-term basis to provide strategic guidance and hands-on execution.
                     </p>
 
--- client/src/components/MobileHomePage/Fractional2.jsx
+++ client/src/components/MobileHomePage/Fractional2.jsx
@@ -8,13 +8,13 @@ const Fractional2 = React.memo(function Fractional2() {
                 <div className='relative bg-[#f5f5f5]  z-1 min-h-[80vh] lg:min-h-[100vh] flex flex-col gap-5 md:gap-8 items-center justify-center'>
                  
                   <p className='font-bold text-4xl md:text-4xl  slide-up stagger-1 !leading-tight ' ref={useViewportAnimation()}
-                          style={{ fontSize: "clamp(30px, 5vw, 57px)" }}
+                          style={{ fontSize: "clamp(30px, 4vw, 57px)" }}
 >
                     What does the fractional marketing consultant package include?
                   </p>
     
                   <p className=' text-3xl font-semibold md:text-3xl   text-[#181818] slide-up stagger-2' ref={useViewportAnimation()}
-                   style={{ fontSize: "clamp(18px, 5.5vw, 40px)" }}>
+                   style={{ fontSize: "clamp(18px, 4vw, 40px)" }}>
                     My <Highlight className=' text-white leading-tight '> fractional marketing consulting package </Highlight> includes everything from strategy to execution:
                   </p>
                                {/* <button> <ArrowBigDown size={40} fill='#911c28' color='#911c28' /></button> */}
