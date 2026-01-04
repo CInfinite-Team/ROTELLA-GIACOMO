98343bdc67e91f1f9a5ffa3dd3df65df16e62f55
gajju44 <gajjunaphade@gmail.com>
2026-01-04 22:14:21 +0530

    updated loading

--- client/src/components/sharedComponents/Loading.jsx
+++ client/src/components/sharedComponents/Loading.jsx
@@ -42,35 +42,52 @@ const Loading = () => {
                         }}
                     />
                     
-                    {/* Text Drawing Effect */}
-                    <motion.text
-                        x="50%"
-                        y="52%" 
-                        textAnchor="middle" 
-                        dominantBaseline="middle" 
-                        fontSize="42" 
-                        fontWeight="700"
-                        fontFamily="'Montserrat', sans-serif"
-                        fill="transparent"
-                        stroke="#911c28"
-                        strokeWidth="1.5"
-                        initial={{ strokeDasharray: 200, strokeDashoffset: 200 }}
-                        animate={{ strokeDashoffset: 0 }}
-                        transition={{
-                            duration: 2,
-                            ease: "easeInOut",
-                            repeat: Infinity,
-                            repeatType: "reverse", 
-                            repeatDelay: 0.5
-                        }}
-                        style={{ letterSpacing: '0.05em' }}
-                    >
-                        RG
-                    </motion.text>
-                    
-                    {/* Text Fill Fading In (Optional: uncomment if solid fill preferred after draw) */}
-                    {/* 
-                    <motion.text
+                    <defs>
+                        <mask id="rg-mask">
+                            <g transform="translate(-5, 0)">
+                                {/* Letter R Mask Path */}
+                                <motion.path
+                                    d="M 50 54 V 86 M 50 54 H 60 C 72 54 72 70 60 70 H 50 M 58 70 L 70 86"
+                                    fill="none"
+                                    stroke="white"
+                                    strokeWidth="12"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    initial={{ pathLength: 0 }}
+                                    animate={{ pathLength: 1 }}
+                                    transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        repeat: Infinity,
+                                        repeatType: "reverse",
+                                        repeatDelay: 0.5
+                                    }}
+                                />
+                                
+                                {/* Letter G Mask Path */}
+                                <motion.path
+                                    d="M 102 60 C 98 48 78 48 78 70 C 78 92 98 92 102 80 V 72 H 92"
+                                    fill="none"
+                                    stroke="white"
+                                    strokeWidth="12"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    initial={{ pathLength: 0 }}
+                                    animate={{ pathLength: 1 }}
+                                    transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        repeat: Infinity,
+                                        repeatType: "reverse",
+                                        repeatDelay: 0.5
+                                    }}
+                                />
+                            </g>
+                        </mask>
+                    </defs>
+
+                    {/* Solid Text with Mask */}
+                    <text
                         x="50%"
                         y="52%" 
                         textAnchor="middle" 
@@ -80,20 +97,11 @@ const Loading = () => {
                         fontFamily="'Montserrat', sans-serif"
                         fill="#911c28"
                         stroke="none"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{
-                            duration: 0.5,
-                            delay: 2,
-                            repeat: Infinity,
-                            repeatType: "reverse",
-                            repeatDelay: 0.5
-                        }}
+                        mask="url(#rg-mask)"
                         style={{ letterSpacing: '0.05em' }}
                     >
                         RG
-                    </motion.text>
-                    */}
+                    </text>
                 </svg>
             </div>
         </div>
@@ -101,3 +109,5 @@ const Loading = () => {
 };
 
 export default Loading;
+
+
